7aa748e25162ce37c52d456756cf1f7d9439fb1d
quang125 <nguyenngocquang296@gmail.com>
2023-10-11 02:51:21 -0700

    alo

---
+++ src/main/java/com/example/querygenerate/exception/RedshiftException.java
@@ -0,0 +1,10 @@
+package com.example.querygenerate.exception;
+
+/**
+ * @author QuangNN
+ */
+public class RedshiftException extends RuntimeException{
+    public RedshiftException(String message){
+
+    }
+}
--- src/main/java/com/example/querygenerate/service/TestLoadService.java
+++ src/main/java/com/example/querygenerate/service/TestLoadService.java
@@ -1,16 +1,6 @@
 package com.example.querygenerate.service;
-
-import com.example.querygenerate.data.Dim;
-import com.example.querygenerate.data.Fact;
-import com.example.querygenerate.utils.QueryGenerateUtils;
-import com.google.gson.Gson;
-import jakarta.annotation.PostConstruct;
-
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  * @author QuangNN
@@ -18,19 +8,63 @@ import java.util.logging.Logger;
 public class TestLoadService {
     private static final RedshiftService redshiftRA3Service = new RedshiftService("jdbc:redshift://test-dwh-cluster.cbyg0igfhhw3.us-east-1.redshift.amazonaws.com:5439/dwh_games", "admin", "mj4Yl9O37GuWxSwLz0Wjs3DJ7");
 
-    public static void boastLoadRa3(){
+    public static void stressTestRa3Dwh_1(){
         Random rand = new Random();
         List<String> table = Arrays.asList("api_ads_log_raw_data_2023_09_25", "api_ads_log_raw_data_2023_10_02", "api_inapp_log_raw_data_2023_09_25",
                 "api_resource_log_raw_data_2023_08_14", "api_resource_log_raw_data_2023_08_21","api_resource_log_raw_data_2023_09_04");
         List<Thread> ra3Threads = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
+        for (int i = 0; i < 12; i++) {
             String fact = table.get(rand.nextInt(table.size()));
             ra3Threads.add(new Thread(() -> {
                 try {
                     while(true) {
-                        String query = "select * from dwh_falcon_1."+fact+" limit 142";
+                        String query = "select * from dwh_falcon_1."+fact+" limit 100000";
+                        String query1 = "select * from dwh_falcon_1."+fact+" limit 100000";
+                        String query2 = "select * from dwh_falcon_1."+fact+" limit 100000";
+                        String query3 = "select * from dwh_falcon_1."+fact+" limit 100000";
+                        String query4 = "select * from dwh_falcon_1."+fact+" limit 100000";
+                        System.out.println(query1);
+                        redshiftRA3Service.excuteSelect(query);
+                        redshiftRA3Service.excuteSelect(query1);
+                        redshiftRA3Service.excuteSelect(query2);
+                        redshiftRA3Service.excuteSelect(query3);
+                        redshiftRA3Service.excuteSelect(query4);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }));
+        }
+        for (Thread ra3Thread : ra3Threads) {
+            ra3Thread.start();
+        }
+        try {
+            for (Thread ra3Thread : ra3Threads) ra3Thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void stressTestRa3Dwh_test(){
+        Random rand = new Random();
+        List<String> table = Arrays.asList("api_ads_log_raw_data", "api_funnel_raw_data", "api_inapp_log_raw_data",
+                "api_level_log_raw_data", "api_property_raw_data","api_resource_log_raw_data");
+        List<Thread> ra3Threads = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            String fact = table.get(rand.nextInt(table.size()));
+            ra3Threads.add(new Thread(() -> {
+                try {
+                    while(true) {
+                        String query = "select  * from dwh_test."+fact+" limit 100000";
+                        String query1 = "select * from dwh_test."+fact+" limit 100000";
+                        String query2 = "select * from dwh_test."+fact+" limit 100000";
+                        String query3 = "select * from dwh_test."+fact+" limit 100000";
+                        String query4 = "select * from dwh_test."+fact+" limit 100000";
                         System.out.println(query);
                         redshiftRA3Service.excuteSelect(query);
+                        redshiftRA3Service.excuteSelect(query1);
+                        redshiftRA3Service.excuteSelect(query2);
+                        redshiftRA3Service.excuteSelect(query3);
+                        redshiftRA3Service.excuteSelect(query4);
                     }
                 } catch (Exception e) {
                     throw new RuntimeException(e);
@@ -47,6 +81,6 @@ public class TestLoadService {
         }
     }
     public static void main(String[] args) {
-        boastLoadRa3();
+        stressTestRa3Dwh_test();
     }
 }
